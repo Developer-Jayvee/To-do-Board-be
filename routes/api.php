4b1d29a5c7a5ec8b954d9cf66805a99cd80944b4
Jayvee <jayvee.github0703@gmail.com>
2026-03-27 13:12:16 +0800

    Setup api endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    Route::post('login',[AuthController::class, 'login']);
+    Route::post('register',[AuthController::class, 'register']);
+
+    Route::middleware(['auth:api'])->group(function () {
+        Route::apiResource('ticket',TicketsController::class);
+        Route::apiResource('categories',CategoriesController::class);
+    });
+});
